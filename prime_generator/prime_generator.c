a96783950a1a526cd5c23afbbb728430805cc030
Nicolás Vega <m.nicolas.vega@gmail.com>
2022-07-18 23:56:11 -0300

    * Replace printing of prime numbers by collecting them in an array in method 'generatePrimes'.   * Add method 'printResult' to print an array of primes.   * Adjust 'main' to changes and make it generate 100k prime numbers.

--- prime_generator/prime_generator.c
+++ prime_generator/prime_generator.c
@@ -25,16 +25,38 @@ boolean isPrime(unsigned long n) {
 	return true;
 }
 
-void generatePrimes(unsigned long start, unsigned long end) {
-	unsigned long n, m;
-	for (n = min(2, start); n <= end; n ++) {
+void generatePrimes(unsigned long start, unsigned long end, unsigned long array_result[], int array_size) {
+	unsigned long n = min(2, start);
+	int i = 0;
+	while (n <= end && i < array_size) {
 		if (isPrime(n)) {
-			printf("%ld, ", n);
+			array_result[i] = n;
+			i ++;
 		}
+		n ++;
+	}
+	while (i < array_size) {
+		array_result[i] = 0;
+		i ++;
 	}
 }
 
+void printResult(unsigned long array_result[], int size, char separator[], int last_digits) {
+	int i = 0;
+	if (last_digits > 0) {
+		i = size - last_digits;
+	}
+	printf("Printing %d results:\n", (size - i));
+	for ( ; i < size; i ++) {
+		printf("%ld%s", array_result[i], separator);
+	}
+	printf("\n");
+}
+
 int main() {
-	generatePrimes(0, 100);
+	unsigned long result[100000];
+	int size = sizeof (result) / sizeof (unsigned long);
+	generatePrimes(0, 1000 * 1000 * 1000, result, size);
+	printResult(result, size, ", ", 10);
 	return 0;
 }
